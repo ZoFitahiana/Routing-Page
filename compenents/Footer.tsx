0c8ea537ab444dd506f1cb8c025c2a0b27d84f4f
ZoFitahiana <hei.tsiaroniaina@gmail.com>
2024-03-11 08:57:59 +0100

    feat : Create components footer and headers

---
+++ compenents/Footer.tsx
@@ -0,0 +1,10 @@
+export function Footer() {
+    return (
+      <footer className="bg-gray-800 text-white py-4">
+        <div className="container mx-auto flex justify-center items-center">
+          <p className="text-sm">© 2024 Blog generated .</p>
+        </div>
+      </footer>
+    );
+  }
+  
---
+++ compenents/Header.tsx
@@ -0,0 +1,10 @@
+export function Header() {
+    return (
+      <div className="bg-blue-500 text-white py-4">
+        <div className="container mx-auto flex justify-center items-center">
+          <h1 className="text-xl font-bold"> Blog : </h1>
+        </div>
+      </div>
+    );
+  }
+  
