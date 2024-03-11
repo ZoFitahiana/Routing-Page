188003191852458191635dd985c54a8a821e02c8
ZoFitahiana <hei.tsiaroniaina@gmail.com>
2024-03-11 09:58:01 +0100

    fix : Add heder and footer fot page home & blog

--- src/app/home/page.tsx
+++ src/app/home/page.tsx
@@ -1,10 +1,14 @@
 import { DataMocks } from "../../../utils/data";
 import { ImplementData } from "../../../compenents/blog";
+import { Header } from "../../../compenents";
+import { Footer } from "../../../compenents";
 
 export default function page() {
   return (
     <div>
+        <Header/>
         {DataMocks.map((blog) => <ImplementData key={blog.id} blog={blog} />)}
+        <Footer/>
     </div>
   )
   
