a0a860377d826aee630a18a0fa72046a448d4a3d
ZoFitahiana <hei.tsiaroniaina@gmail.com>
2024-03-11 08:44:29 +0100

    feat : Create data mock to implement

---
+++ utils/data.ts
@@ -0,0 +1,69 @@
+export type DataMock = {
+    id: string,
+    title: string,
+    content: string,
+    img: string
+  }
+
+export const DataMocks: DataMock[] = [
+    {
+      id: "1",
+      title: "Les bienfaits de la méditation",
+      content: "La méditation est une pratique ancestrale qui offre de nombreux bienfaits pour le corps et l'esprit. Elle permet de réduire le stress, d'améliorer la concentration et de favoriser le bien-être général.",
+      img: "https://picsum.photos/200/300"
+    },
+    {
+      id: "2",
+      title: "Recette de smoothie santé",
+      content: "Découvrez une délicieuse recette de smoothie aux fruits frais pour faire le plein de vitamines et d'énergie. Ce smoothie est parfait pour un petit-déjeuner équilibré ou une collation saine.",
+      img: "https://source.unsplash.com/random/200x300"
+    },
+    {
+      id: "3",
+      title: "Conseils pour un sommeil réparateur",
+      content: "Le sommeil est essentiel pour notre santé et notre bien-être. Dans cet article, nous vous donnons quelques conseils pour améliorer la qualité de votre sommeil et vous réveiller chaque matin frais et reposé.",
+      img: "https://placeimg.com/200/300/any"
+    },
+    {
+      id: "4",
+      title: "Les bienfaits de l'exercice physique",
+      content: "L'exercice physique régulier est crucial pour maintenir une bonne santé. Il aide à renforcer les muscles, à améliorer la circulation sanguine et à réduire le risque de maladies cardiaques.",
+      img: "https://loremflickr.com/200/300"
+    },
+    {
+      id: "5",
+      title: "Idées de repas équilibrés",
+      content: "Adopter une alimentation équilibrée est essentiel pour rester en bonne santé. Dans cet article, découvrez des idées de repas simples, savoureux et nutritifs pour vous aider à maintenir une alimentation équilibrée.",
+      img: "https://via.placeholder.com/200x300"
+    },
+    {
+      id: "6",
+      title: "Gestion du stress au travail",
+      content: "Le stress au travail peut avoir un impact négatif sur notre santé mentale et physique. Apprenez quelques techniques simples pour gérer le stress au travail et maintenir un équilibre entre vie professionnelle et personnelle.",
+      img: "https://www.fillmurray.com/200/300"
+    },
+    {
+      id: "7",
+      title: "Bienfaits de la lecture",
+      content: "La lecture est une activité enrichissante qui stimule l'esprit et nourrit l'imagination. Découvrez dans cet article les nombreux bienfaits de la lecture pour notre cerveau et notre bien-être général.",
+      img: "https://www.placecage.com/200/300"
+    },
+    {
+      id: "8",
+      title: "Pratiquer la gratitude au quotidien",
+      content: "La gratitude est une pratique puissante qui peut transformer notre vie et notre perception du monde. Apprenez comment cultiver la gratitude au quotidien et profiter des nombreux bienfaits qu'elle apporte.",
+      img: "https://www.stevensegallery.com/200/300"
+    },
+    {
+      id: "9",
+      title: "Exercices de relaxation pour le bien-être",
+      content: "La relaxation est essentielle pour réduire le stress et favoriser le bien-être. Dans cet article, découvrez quelques exercices simples de relaxation pour vous aider à vous détendre et à retrouver l'équilibre intérieur.",
+      img: "https://placekitten.com/200/300"
+    },
+    {
+      id: "10",
+      title: "Voyager pour élargir ses horizons",
+      content: "Voyager est une expérience enrichissante qui permet de découvrir de nouvelles cultures, de rencontrer des personnes inspirantes et d'explorer des paysages magnifiques. Découvrez dans cet article l'importance de voyager pour élargir ses horizons.",
+      img: "https://www.placebear.com/200/300"
+    },
+  ];
