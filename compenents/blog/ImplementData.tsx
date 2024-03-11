16a5de86f0c2f5c2e9198c457d480d45056565e0
ZoFitahiana <hei.tsiaroniaina@gmail.com>
2024-03-11 09:03:05 +0100

    feat : Create function to receive data

---
+++ compenents/blog/ImplementData.tsx
@@ -0,0 +1,32 @@
+import { DataMock } from "../../utils/data";
+import Image from "next/image";
+import Link from "next/link";
+type itemProps = {
+  blog: DataMock;
+  showImage?: boolean;
+  showContent?: boolean;
+};
+
+export function Item({ blog, showContent, showImage }: itemProps) {
+    return (
+      <Link href={`blog/${blog.id}`}>
+        <div className="w-full mx-auto p-5 max-w-[800px] bg-gray-200 my-2 rounded-lg shadow-md">
+          <p className="text-center font-bold text-xl mb-2">{blog.title}</p>
+          {showContent && (
+            <div>
+              <p className="text-center text-gray-700">{blog.content}</p>
+              <Link href={`blog/${blog.id}/image`} className="block text-center mt-2 text-blue-500 hover:underline font-bold">
+                Voir l'image
+              </Link>
+            </div>
+          )}
+          {showImage && (
+            <div className="mt-4">
+              <Image src={blog.img} alt="bllogImage" height={400} width={500} className="rounded-lg" />
+            </div>
+          )}
+        </div>
+      </Link>
+    );
+  }
+  
